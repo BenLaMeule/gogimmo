76158b98dcd522db3df1b6d13c2715c17790fa81
Benjamin Meuleman <benjamin.meuleman@gmail.com>
2016-06-14 15:33:19 +0200

    avancées sur Se Loger

--- db/seeds.rb
+++ db/seeds.rb
@@ -31,3 +31,37 @@ for i in 1..15
     end
   puts "Page #{i}/15 completed"
 end
+
+# SE LOGER
+# Bordeaux
+puts "SE LOGER - Bordeaux"
+for i in 1..15
+  url = "http://www.seloger.com/list.htm?ci=330063&idtt=2&idtypebien=2,13,14,1,9&LISTING-LISTpg=#{i}"
+  html_file = open(url)
+  html_doc = Nokogiri::HTML(html_file)
+  html_doc.search('.listing.life_annuity.gold').each do |element|
+    price = element.search('.listing_infos .price .amount').text.gsub(/\D|\u0082/,"").to_i
+    surface = element.search('.listing_photo_container .property_list').children[2].text.gsub(/\D/,"").to_i
+    room = element.search('.listing_photo_container .property_list').children[0].text.gsub(/\D/,"").to_i
+    # city =
+    # zip_code =
+    # sector =
+    website = "Se-Loger"
+    prix_metre_carre = price / surface
+    # maj = html_doc.search('.offer-picture .offer-update').first.children.text.match(/(0[1-9]|[1-2][0-9]|3[0-1])\/(0[1-9]|1[0-2])\/[0-9]{4}/)[0].to_date
+    Annonce.create(price: price, surface: surface, room: room, website: website, prix_metre_carre: prix_metre_carre)
+  end
+  # html_doc.search('.listing.life_annuity.silver').each do |element|
+  #   price =
+  #   surface =
+  #   room =
+  #   city =
+  #   zip_code =
+  #   sector =
+  #   website = "Se-Loger"
+  #   prix_metre_carre = price / surface
+  #   maj = html_doc.search('.offer-picture .offer-update').first.children.text.match(/(0[1-9]|[1-2][0-9]|3[0-1])\/(0[1-9]|1[0-2])\/[0-9]{4}/)[0].to_date
+  #   Annonce.create(price: price, surface: surface, room: room, city: city, zip_code: zip_code, sector: sector, maj: maj, website: website, prix_metre_carre: prix_metre_carre)
+  # end
+  puts "Page #{i}/15 completed"
+end
